12ba3a52e871764e77e32b9474f53645c9a69701
HoEmpire <541218251@qq.com>
2020-04-20 21:21:54 +0800

    start of multi clouds fusion

--- src/multi_pointcloud.cpp
+++ src/multi_pointcloud.cpp
@@ -14,8 +14,12 @@
 #include <pcl_ros/point_cloud.h>
 
 #include "pointcloud_fusion/util.h"
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
 
 int global_flag = 1;
+std::vector<pcl::PointCloud<pcl::PointXYZRGB>> clouds;
 
 void callback(const sensor_msgs::PointCloud2ConstPtr &msg_pc,
               const sensor_msgs::ImageConstPtr &msg_img)
@@ -34,12 +38,33 @@ void callback(const sensor_msgs::PointCloud2ConstPtr &msg_pc,
         cv_ptr = cv_bridge::toCvCopy(msg_img, sensor_msgs::image_encodings::BGR8);
 
         point_cloud_color = paintPointCloud(point_cloud_livox, cv_ptr->image);
-        pcl::io::savePCDFile("/home/tim/fuse_pc.pcd", point_cloud_color);
+        clouds.push_back(point_cloud_color);
+        pcl::io::savePCDFile("/home/tim/pc.pcd", point_cloud_color);
         global_flag = 0;
     }
     else if (global_flag == 2)
     {
+        pcl::PointCloud<pcl::PointXYZRGB> origin = clouds[0];
         ROS_INFO("Fusing point clouds");
+        for (int i = 1; i < clouds.size(); i++)
+        {
+            pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> icp;
+            icp.setInputTarget(origin.makeShared());
+            icp.setInputSource(clouds[i].makeShared());
+            // Set the max correspondence distance to 1cm (e.g., correspondences with higher distances will be ignored)
+            icp.setMaxCorrespondenceDistance(0.01);
+            // Set the maximum number of iterations (criterion 1)
+            icp.setMaximumIterations(100);
+            pcl::PointCloud<pcl::PointXYZRGB> Final;
+            icp.align(Final);
+            ROS_INFO_STREAM("ICP has converged?: " << icp.hasConverged());
+            std::cout << "Final Transformation: " << std::endl
+                      << icp.getFinalTransformation() << std::endl;
+            pcl::PointCloud<pcl::PointXYZRGB> new_cloud;
+            pcl::transformPointCloud(clouds[i], new_cloud, icp.getFinalTransformation());
+            origin += new_cloud;
+        }
+        pcl::io::savePCDFile("/home/tim/result_icp.pcd", origin);
         ROS_INFO("Fusion Complete!!");
         ros::shutdown();
     }
