655812fd8146efe0573eeb425adda0295f9b7cab
HoEmpire <541218251@qq.com>
2020-06-09 17:31:11 +0800

    add statistical filter

--- mapping_wo_ros/include/util.h
+++ mapping_wo_ros/include/util.h
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <vector>
 
+#include <pcl/filters/statistical_outlier_removal.h>  //统计滤波器头文件
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_cloud.h>
+
 #include <Eigen/Core>
 #include <Eigen/LU>
 #include <opencv/cv.hpp>
@@ -112,10 +114,16 @@ void readData(std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> &pcs, std::vec
   }
 
   infile.close();
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> statisticalFilter;  //创建滤波器对象
+  statisticalFilter.setMeanK(50);                                      //取平均值的临近点数
+  statisticalFilter.setStddevMulThresh(1);  //超过平均距离一个标准差以上，该点记为离群点，将其移除
   for (int i = 0; i < data_len; i++)
   {
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr tmp(new pcl::PointCloud<pcl::PointXYZRGB>());
     pcl::io::loadPCDFile<pcl::PointXYZRGB>(config.data_path + std::to_string(i) + ".pcd", *tmp);
+    statisticalFilter.setInputCloud(tmp);  //设置待滤波的点云
+    statisticalFilter.filter(*tmp);        //执行滤波处理，保存内点到cloud_after_StatisticalRemoval
+
     pcs.push_back(tmp);
     imgs.push_back(cv::imread(config.data_path + std::to_string(i) + ".jpg"));
     depths.push_back(cv::imread(config.data_path + std::to_string(i) + ".png", CV_16UC1));
